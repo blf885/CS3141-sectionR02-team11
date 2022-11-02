5c564a0ff788639c88a3d2ba662191b1e8af1bd1
blf885 <98063996+blf885@users.noreply.github.com>
2022-11-02 12:29:55 -0400

    Trick class and trick class test updated

--- project_knievel/test/trick_class_tests.dart
+++ project_knievel/test/trick_class_tests.dart
@@ -1,10 +1,34 @@
 import 'package:test/test.dart';
-import 'package:project_knievel/main.dart';
+import 'package:project_knievel/Trick.dart';
 
 void main() {
+  // Test Trick.setName
   test('Test Trick constructor and Trick.setName method', () {
     Trick theTrick = Trick(
         'Tailwhip', 'The deck spins around 360', 1, 'theLink.com', true, 1);
     expect(theTrick.setName('Tail-whip'), equals('Tailwhip'));
+  });
+
+  // Test Trick.setDesciption
+  test('Test Trick constructor and Trick.setDesciption method', () {
+    Trick theTrick = Trick(
+        'Tailwhip', 'The deck spins around 360', 1, 'theLink.com', true, 1);
+    expect(theTrick.setDescription('The deck is kicked around 360'),
+        equals('The deck spins around 360'));
+  });
+
+  // Test Trick.setDifficulty
+  test('Test Trick constructor and Trick.setDifficulty method', () {
+    Trick theTrick = Trick(
+        'Tailwhip', 'The deck spins around 360', 2, 'theLink.com', true, 1);
+    expect(theTrick.setDifficulty(2), equals(2));
+  });
+
+  // Test Trick.setHowTo
+  //TODO finish this
+  test('Test Trick constructor and Trick.setDifficulty method', () {
+    Trick theTrick = Trick(
+        'Tailwhip', 'The deck spins around 360', 2, 'theLink.com', true, 1);
+    expect(theTrick.setDifficulty(2), equals(2));
   });
 }
