1b46da7db714d1298f1cda6007541577d3b7e163
yhernand626 <72482697+yhernand626@users.noreply.github.com>
2022-11-11 12:17:35 -0500

    Added the last of the Testing in Trick List Test file

--- project_knievel/test/Trick_List_test.dart
+++ project_knievel/test/Trick_List_test.dart
@@ -20,4 +20,10 @@ void main() {
         1, 1); // list temp and many 1's are temp placeholders
     expect(trick.getSize(), equals(2));
   });
+
+  test('Testing Tricklist constructor and Tricklist.getSize method ', () {
+    TrickList trick = TrickList('Tailwhip', 'ListTemp', 2, 1, 1, 1, 1, 1, 1, 1,
+        1, 1); // list temp and many 1's are temp placeholders
+    expect(trick.setSize(2), equals(2));
+  });
 }
