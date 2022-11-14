90e5dd4538a72c952b96e7dd6fc159bae0a15b1e
yhernand626 <72482697+yhernand626@users.noreply.github.com>
2022-11-13 21:53:18 -0500

    Removed redundant files

--- project_knievel/test/class_tests.dart
+++ project_knievel/test/class_tests.dart
@@ -1,3 +1,4 @@
+import 'package:project_knievel/TrickList.dart';
 import 'package:test/test.dart';
 import 'package:project_knievel/Trick.dart';
 
@@ -80,4 +81,30 @@ void main() {
 
   //---------------------------------------------------------------------------
   // Feature Class Tests
+
+  // Trick List class Tests
+  // Test TrickList.setName
+  test('Test Trick_List constructor and Trick_List.setName method', () {
+    TrickList trick = TrickList('Tailwhip', 'ListTemp', 2, 1, 1, 1, 1, 1, 1, 1,
+        1, 1); // list temp and many 1's are temp placeholders
+    expect(trick.setName('Tail-whip'), equals('Tailwhip'));
+  });
+
+  test('Testing Tricklist constructor and Tricklist.getName method ', () {
+    TrickList trick = TrickList('Tailwhip', 'ListTemp', 2, 1, 1, 1, 1, 1, 1, 1,
+        1, 1); // list temp and many 1's are temp placeholders
+    expect(trick.getName(), equals('Tailwhip'));
+  });
+
+  test('Testing Tricklist constructor and Tricklist.getSize method ', () {
+    TrickList trick = TrickList('Tailwhip', 'ListTemp', 2, 1, 1, 1, 1, 1, 1, 1,
+        1, 1); // list temp and many 1's are temp placeholders
+    expect(trick.getSize(), equals(2));
+  });
+
+  test('Testing Tricklist constructor and Tricklist.getSize method ', () {
+    TrickList trick = TrickList('Tailwhip', 'ListTemp', 2, 1, 1, 1, 1, 1, 1, 1,
+        1, 1); // list temp and many 1's are temp placeholders
+    expect(trick.setSize(2), equals(2));
+  });
 }
