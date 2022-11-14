3826766135307a388258ffcfd35c5cd9c530be3c
yhernand626 <72482697+yhernand626@users.noreply.github.com>
2022-11-13 21:50:56 -0500

    Updated file name

--- project_knievel/test/Trick_List_test.dart
+++ project_knievel/test/Trick_List_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:project_knievel/TrickListClass.dart';
+import 'package:project_knievel/TrickList.dart';
 
 void main() {
   // Test TrickList.setName
